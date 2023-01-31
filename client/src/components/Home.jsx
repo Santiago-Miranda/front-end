6f3311cda2e6bbc18987a1c35ab6405dd5a94c5f
Santiago Miranda <SantyMiranda101@gmail.com>
2023-01-31 15:35:45 -0300

    Update Home.jsx

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -56,7 +56,7 @@ export default function Home(){
             <div className='home-nav'>
                 <div className='home-navbar-up'>
                     <SearchBar/>
-                    <h2>Recetas</h2>
+                    <h1>RECETAS</h1>
                     <Link to='/recipecreate'>
                         <button id='crear-receta'>
                             <h1>CREAR RECETA</h1>
